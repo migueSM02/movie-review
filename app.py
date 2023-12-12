52ff251341ec07125a6b52e93f9ef5caca49fe86
Miguel Angel <miguel.as02@hotmail.com>
2023-12-11 19:30:31 -0500

    add comments to code

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 from flask import Flask, render_template, request, jsonify
-#from flask_cors import CORS
 import numpy as np
 import json
 import joblib
 from googletrans import Translator
 
 app = Flask(__name__)
-
 translator = Translator()
 
+
+#cargar el diccionario y el modelo entrenado
 with open('data/imbd_dic.json', 'r') as f:
     word_index = json.load(f)
 
 trained_model = joblib.load('data/trained_model.pkl')
 
+
 @app.route('/static/<path:path>')
 def static_file(path):
     return app.send_static_file(path)
+
 
 def vectorize_sequences_with_dtype(sequences, dimension=10000, dtype=np.float32):
     results = np.zeros((len(sequences), dimension), dtype=dtype)
@@ -25,16 +27,20 @@ def vectorize_sequences_with_dtype(sequences, dimension=10000, dtype=np.float32)
         results[i, valid_indices] = 1.
     return results
 
+
 def encode_review(text, word_index, max_words=10000):
     tokens = text.split()
     sequence = [word_index.get(word, 0) for word in tokens if word_index.get(word, 0) < max_words]
     return sequence
 
 
+# Ruta principal para la página de inicio
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+# Ruta para la predicción basada en la entrada del usuario
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
@@ -49,9 +55,7 @@ def predict():
         else:
             encoded_review = encode_review(review_text, word_index)
 
-        #encoded_review = encode_review(review_text, word_index)
         vectorized_review = vectorize_sequences_with_dtype([encoded_review])
-
         predicted_label = trained_model.predict(vectorized_review)[0]
 
         if int(predicted_label) == 0:
@@ -60,7 +64,6 @@ def predict():
             predicted_text = "Positive"
         return jsonify({'prediction': predicted_text})
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-# python app.py
